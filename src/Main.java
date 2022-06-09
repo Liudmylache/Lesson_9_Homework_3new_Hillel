fb6c8478b65cd949cd776c939709af7a026580fd
Lucy <lucydenisenko@gmail.com>
2022-06-09 12:33:42 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        printIntructions();
+        String input = bufferedReader.readLine();
+
+        String newString = input.toUpperCase().toLowerCase().replace(" ", "");
+        String newReversedString = Main.reverseString(newString);
+
+        System.out.println("Your text is: " + newReversedString);
+
+        if (newReversedString.equals(input)) {
+            isPalindromeMessage();
+            return;
+        }
+        isNotPalindromeMessage();
+        return;
+    }
+
+    public static void printIntructions() {
+        System.out.println("Type a palindrom.");
+        System.out.println("(ex 'a roza upala na lapu azora')");
+    }
+
+   public static String reverseString(String newString){
+        if (newString.length() <= 1) {
+            return newString;
+       }
+        return (reverseString(newString.substring(1)) + newString.charAt(0));
+   }
+
+   public static void isPalindromeMessage() {
+        System.out.println("Wow! It is a palindrome. Congrats!");
+   }
+
+   public static void isNotPalindromeMessage() {
+        System.out.println("No, it is not a palindrome :(");
+    }
+
+
+}
